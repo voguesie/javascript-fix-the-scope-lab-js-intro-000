7448d020dd24b675318ac109874b7b9ce9d5822a
Michael Vogel <voguesie@alumni.stanford.edu>
2019-11-28 04:09:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ var animal = 'cat'
   return animal
 }
 
+const two = 2
 function add2(n) {
   return n + two
 
   // Feel free to move things around!
-  const two = 2
 }
 
 var funkyFunction = function() {
